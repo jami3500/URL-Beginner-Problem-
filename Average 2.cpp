357cc4543b574b1266a0223d0f9724400adc364c
jahid hasan <107705267+jami3500@users.noreply.github.com>
2024-03-09 22:48:09 +0600

    Add files via upload

---
+++ Average 2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    float a,b,c,avg;
+    cin >> a >> b >> c;
+    avg = ((a*2+b*3+c*5)/(2+3+5));
+
+
+    cout << "MEDIA = " << fixed << setprecision(1) << avg << endl;
+
+    return 0;
+}
+
