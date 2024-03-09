4bc888d93d51dce684038c7f5f5164da0c7a0d96
jahid hasan <107705267+jami3500@users.noreply.github.com>
2024-03-09 22:44:37 +0600

    Add files via upload

---
+++ Area of a Circle.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>>
+
+using namespace std;
+
+int main() {
+ double a,r;
+ cin >> r;
+
+ a = 3.14159*r*r;
+
+ cout << "A=" << fixed << setprecision(4) << a << endl;
+
+    return 0;
+}
